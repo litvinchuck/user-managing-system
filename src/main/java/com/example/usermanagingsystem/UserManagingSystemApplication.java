14f328a585d22107ddb96b2b2a16275cb338be59
Eduard Litvinchuk <elitvinchuck@gmail.com>
2023-07-03 19:57:56 +0300

    Spring Boot base

---
+++ src/main/java/com/example/usermanagingsystem/UserManagingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.usermanagingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagingSystemApplication.class, args);
+	}
+
+}
